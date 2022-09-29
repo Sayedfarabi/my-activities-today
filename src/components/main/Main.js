3473367092cdd087bd4442a3064c71920e03470a
Sayedfarabi <sayedfarabi6099@gmail.com>
2022-09-29 19:46:29 +0600

    header section done

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import Activities from '../activities/Activities';
 import Header from '../../components/header/Header';
+import image from '../../said.jpg';
+import Info from '../my-info/Info';
 
 const Main = () => {
     const [activities, setActivities] = useState([]);
@@ -34,7 +36,36 @@ const Main = () => {
             </div>
 
             <div className='col-md-3 shadow'>
-                <h3>Activities Summery</h3>
+                <div className='row m-3'>
+                    <div className='col-sm-3 col-md-4'>
+                        <img className='w-75 h-75 rounded' src={image} alt="My pic" />
+                    </div>
+                    <div className='col-sm-9 col-md-8'>
+                        <h5>Sayed Hasan Farabi</h5>
+                        <address>Dhaka, Bangladesh</address>
+                    </div>
+                </div>
+                <div className='d-flex justify-content-between mx-3'>
+                    <Info
+                        name={"Weight"}
+                        value={"60"}
+                        category={"kg"}>
+                    </Info>
+
+                    <Info
+                        name={"Weight"}
+                        value={"60"}
+                        category={"kg"}>
+                    </Info>
+
+                    <Info
+                        name={"Weight"}
+                        value={"60"}
+                        category={"kg"}>
+                    </Info>
+
+
+                </div>
             </div>
         </div>
     );
---
+++ src/components/my-info/Info.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Info = (props) => {
+    const { value, name, category } = props;
+    return (
+        <div>
+            <div>
+                <p className='fs-3 fw-bolder'>{value} <span className='fs-6 text-muted'>{category}</span></p>
+            </div>
+            <div>
+                <p className='fs-5 text-muted'>{name}</p>
+            </div>
+        </div>
+
+    );
+};
+
+export default Info;
